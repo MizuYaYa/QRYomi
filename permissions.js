5f465f9250b44a7500370baf706d8595dbbf0fe5
nizya-mirai-macbookair <miraichanneru667@gmail.com>
2022-11-24 16:12:25 +0900

    [update] カメラ自動で停止
    カメラ権限の許可ボタンを押したらカメラがついてしまうので念の為カメラを自動で落とすようにしました。

--- permissions.js
+++ permissions.js
@@ -1,6 +1,7 @@
 navigator.mediaDevices.getUserMedia({video: true,})
-    .then(() => {
+    .then(stream => {
 		console.log("カメラへのアクセスが許可されました。\nCamera access granted.");
+		stream.getTracks().forEach(track => track.stop());
 	})
     .catch(e => {
 		console.log(`カメラへのアクセスがブロックされました。\nCamera access blocked.\n${e}`);
