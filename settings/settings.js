d275f2365fbc6597dcdb80284b3a49db03c38643
nizya-mirai-macbookair <miraichanneru667@gmail.com>
2022-12-19 00:50:09 +0900

    [add] サイドバー開閉
    サイドバーを開閉できるようにしました。

---
+++ settings/settings.js
@@ -0,0 +1,13 @@
+const sidebar_close_btn = document.getElementById("sidebar_close_btn");
+const sidebar_open_btn = document.getElementById("sidebar_open_btn");
+const sidebar = document.getElementById("sidebar");
+
+sidebar_close_btn.addEventListener("click", ()=>{
+    sidebar.classList.add("sidebar_closed");
+    sidebar_open_btn.style.visibility = "visible";
+});
+
+sidebar_open_btn.addEventListener("click", ()=>{
+    sidebar_open_btn.style.visibility = "";
+    sidebar.classList.remove("sidebar_closed");
+});
