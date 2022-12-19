a308001ec8d1b828e9c897cd0cd80c21236907e1
nizya-mirai-macbookair <miraichanneru667@gmail.com>
2022-12-19 20:01:29 +0900

    [update] ユーザー体験の向上
    アラートを使ってカメラアクセスの許可やブロックを伝えるようにしました。

--- permissions.js
+++ permissions.js
@@ -1,8 +1,7 @@
-navigator.mediaDevices.getUserMedia({video: true,})
-    .then(stream => {
+navigator.mediaDevices.getUserMedia({video: true}).then(() => {
 		console.log("カメラへのアクセスが許可されました。\nCamera access granted.");
-		stream.getTracks().forEach(track => track.stop());
-	})
-    .catch(e => {
+		alert("カメラへのアクセスが許可されました！\nこのページを閉じてQRコードを読み込みましょう！")
+}).catch(e => {
 		console.log(`カメラへのアクセスがブロックされました。\nCamera access blocked.\n${e}`);
-	});
+		alert("カメラへのアクセスがブロックされました。\nchrome側のサイトの設定からカメラの権限を許可してください。")
+});
