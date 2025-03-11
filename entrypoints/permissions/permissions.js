3eddea39271b733a4e768804873a3e346ee60500
MizuYaYa <106097784+MizuYaYa@users.noreply.github.com>
2025-03-11 14:31:51 +0900

    chore: biomeで修正

--- entrypoints/permissions/permissions.js
+++ entrypoints/permissions/permissions.js
@@ -1,10 +1,10 @@
-let cam_permission_desc = document.getElementById("cam_permission_desc");
+const cam_permission_desc = document.getElementById("cam_permission_desc");
 
 //言語ファイルに格納されている文字列を取得
 function getMessage(msg) {
-	let getMsg = chrome.i18n.getMessage(msg);
-	console.log(`msg: "${msg}" \ngetMsg: "${getMsg}"`);
-	return getMsg;
+  const getMsg = chrome.i18n.getMessage(msg);
+  console.log(`msg: "${msg}" \ngetMsg: "${getMsg}"`);
+  return getMsg;
 }
 
 //cam_permission_descに「QRコードを読み込むには、カメラのアクセス許可が必要です。左上の「許可する」をクリックしてください。」を表示させる
@@ -12,15 +12,13 @@ cam_permission_desc.innerText = getMessage("permissions_cam_access_permission");
 
 document.title = getMessage("permissions_title");
 
-navigator.mediaDevices.getUserMedia({video: true})
-.then(() => {
-
+navigator.mediaDevices
+  .getUserMedia({ video: true })
+  .then(() => {
     console.log("カメラへのアクセスが許可されました。\nCamera access granted.");
     alert(getMessage("permissions_cam_access_granted"));
-
-}).catch(e => {
-
+  })
+  .catch((e) => {
     console.log(`カメラへのアクセスがブロックされました。\nCamera access blocked.\n${e}`);
     alert(getMessage("permissions_cam_access_blocked"));
-
-});
+  });
