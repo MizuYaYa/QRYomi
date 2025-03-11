692434a9dfe4fc8e55700ae224c09a3218de8fae
MizuYaYa <106097784+MizuYaYa@users.noreply.github.com>
2025-03-11 14:03:15 +0900

    fix: packageのjsQRに移行

--- entrypoints/popup/popup.js
+++ entrypoints/popup/popup.js
@@ -1,3 +1,5 @@
+import jsQR from "jsqr";
+
 const video = document.createElement("video");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d", { willReadFrequently: true});
